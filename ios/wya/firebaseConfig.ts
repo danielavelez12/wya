8773bd1741c6650d3699f262e95fda102581b1e6
Daniela Velez <daniela@alza.app>
2024-09-02 18:42:28 -0400

    firebase config

--- ios/wya/firebaseConfig.ts
+++ ios/wya/firebaseConfig.ts
@@ -1,4 +1,4 @@
-import { initializeApp } from 'firebase/app';
+import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 // Optionally import the services that you want to use
@@ -8,20 +8,20 @@ import { getFirestore } from "firebase/firestore";
 // import {...} from "firebase/functions";
 // import {...} from "firebase/storage";
 
-// Initialize Firebase
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: 'project-id.firebaseapp.com',
-  databaseURL: 'https://project-id.firebaseio.com',
-  projectId: 'project-id',
-  storageBucket: 'project-id.appspot.com',
-  messagingSenderId: 'sender-id',
-  appId: 'app-id',
-  measurementId: 'G-measurement-id',
+  authDomain: "wya-app-530d5.firebaseapp.com",
+  projectId: "wya-app-530d5",
+  storageBucket: "wya-app-530d5.appspot.com",
+  messagingSenderId: "663230930206",
+  appId: "1:663230930206:web:8954c9da3ca0c6ea8d3d0f",
+  measurementId: "G-TW3P6945TN",
 };
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
-export default app; 
+export default app;
 // For more information on how to access Firebase in your project,
 // see the Firebase documentation: https://firebase.google.com/docs/web/setup#access-firebase
